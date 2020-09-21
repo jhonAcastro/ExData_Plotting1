ec950dad970afd4b708e1797a22876f2503f9e13
Jhon Alexander <jpenafielc25@gmail.com>
2020-09-20 22:07:17 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,10 @@
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+path <- getwd()
+download.file(url, file.path(path, "dataFiles.zip"))
+unzip(zipfile = "dataFiles.zip")
+library(data.table)
+datos <- data.table::fread(input ="household_power_consumption.txt", na.strings="?")
+datos1 <- datos[datos$Date %in% c("1/2/2007","2/2/2007") ,]
+datos1$Date <- as.Date(datos1$Date, format = "%d/%m/%Y")
+datos1$Time <- strptime(datos1$Time, format = "%H:%M:%S")
+Global_active_power <- as.numeric(datos1$Global_active_power)
