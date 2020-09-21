cb2bc5e9b0e11e90b7422ee9341307cd48a3efac
Jhon Alexander <jpenafielc25@gmail.com>
2020-09-20 22:31:14 -0500

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,26 @@
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+path <- getwd()
+download.file(url, file.path(path, "dataFiles.zip"))
+unzip(zipfile = "dataFiles.zip")
+library(data.table)
+datos <- data.table::fread(input ="household_power_consumption.txt", na.strings="?")
+datos1 <- datos[datos$Date %in% c("1/2/2007","2/2/2007") ,]
+
+fecha <- strptime(paste(datos1$Date, datos1$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+Global_active_power <- as.numeric(datos1$Global_active_power)
+
+Global_reactive_power <- as.numeric(datos1$Global_reactive_power)
+Voltage <- as.numeric(datos1$Voltage)
+sub_Metering_1 <- as.numeric(datos1$Sub_metering_1)
+sub_Metering_2 <- as.numeric(datos1$Sub_metering_2)
+sub_Metering_3 <- as.numeric(datos1$Sub_metering_3)
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+plot(fecha, Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(fecha, Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(fecha, sub_Metering_1, type="l", ylab="Energy Submetering", xlab="")
+lines(fecha, sub_Metering_2, type="l", col="red")
+lines(fecha, sub_Metering_3, type="l", col="blue")
+legend("Topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(fecha, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+dev.off()
